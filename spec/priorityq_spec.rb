8ad7056ebe5315843f260d7c30f438624054d1bb
Les Hill <leshill@gmail.com>
2019-12-11 13:14:44 -0800

    Prepare specs

--- spec/priorityq_spec.rb
+++ spec/priorityq_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Priorityq do
   it 'has a version number' do
     expect(Priorityq::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
